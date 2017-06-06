13c0aa7e37df0fcf5ef5f6171f482bee332e5dfd
Sathish <sathish@cupolatechnology.com>
2017-06-06 19:02:40 +0530

    Adding Progress bar

--- src/components/statusbar/statusbar.ts
+++ src/components/statusbar/statusbar.ts
@@ -1,6 +1,10 @@
 import {dom, quickDom, emptyDom} from '../../dom/dom'
 import {component} from '../../components/component'
 
+import { loadstyle } from "../../load";
+import * as path from 'path'
+loadstyle(path.join(__dirname, './media/statusbar.css'));
+
 export class statusbar extends component {
     // private container: dom;
     constructor (
@@ -12,12 +16,12 @@ export class statusbar extends component {
         }
         this.container = emptyDom().element('div', 'statusbar');
         this.container.apendTo(parent);
+        emptyDom().element('div', 'statusbar-progress').apendTo(this.container);
     }
 
     updateStyle() {
         // style update for statusbar
         super.updateStyle();
         let container = this.getcontainer();
-        container.style('background-color', 'rgba(0, 122, 204, 1)');
     }
 }
