5fb2b3cb5f9da353e91b6f1f0f2618700d600ec9
Sathish <sathish@cupolatechnology.com>
2017-06-06 17:15:17 +0530

    Adding JLink configuration setting

--- src/bootstrap/main.js
+++ src/bootstrap/main.js
@@ -1,9 +1,10 @@
 let workspace = require('../workspace/workspace');
+let jlink = require('../util/jlink');
 
 function load() {
 	// Initialize the workspace
 	let Workspace = new workspace.workspace();
-
+	jlink.configureJlink("/opt/SEGGER/JLink/JLinkExe", "-device NRF52 -if SWD -speed 4000");
 	Workspace.open();
 }
 
